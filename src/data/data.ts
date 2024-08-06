4b147a8362137cc37ad224e2d59986c6cee6fc08
Matías Sandoval <matias.sandoval.pavez@gmail.com>
2024-08-05 22:26:08 -0400

    feat: Add skills section to homepage

---
+++ src/data/data.ts
@@ -0,0 +1,51 @@
+export const SKILLS = [
+	{
+		name: "JavaScript",
+		level: "90%",
+		svg: "https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg",
+	},
+	{
+		name: "React",
+		level: "90%",
+		svg: "https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original.svg",
+	},
+	{
+		name: "Node.js",
+		level: "80%",
+		svg: "https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original.svg",
+	},
+	{
+		name: "MongoDB",
+		level: "80%",
+		svg: "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original.svg",
+	},
+	{
+		name: "GraphQL",
+		level: "70%",
+		svg: "https://raw.githubusercontent.com/devicons/devicon/master/icons/graphql/graphql-plain.svg",
+	},
+	{
+		name: "TypeScript",
+		level: "60%",
+		svg: "https://raw.githubusercontent.com/devicons/devicon/master/icons/typescript/typescript-original.svg",
+	},
+	{
+		name: "Python",
+		level: "50%",
+		svg: "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+	},
+	{
+		name: "Java",
+		level: "40%",
+		svg: "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg",
+	},
+	{
+		name: "C#",
+		level: "70%",
+		svg: "https://raw.githubusercontent.com/devicons/devicon/master/icons/csharp/csharp-original.svg",
+	},
+	{
+		name: "Ruby",
+		level: "10%",
+	},
+];
